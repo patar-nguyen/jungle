17315860bc16663b6a673f6de9b190cf74b7a925
Patrick Nguyen <patar.nguyen@gmail.com>
2022-03-09 20:16:33 -0500

    Implement Admin Dashboard-Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: 'Jungle', password: 'book'
 
   def show
+    @product = Product.count
+    @category = Category.count
   end
 
 end
