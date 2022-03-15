8fa9ef00b0d66b0d2d4bd7f9ae03948498634790
Patrick Nguyen <patar.nguyen@gmail.com>
2022-03-15 16:42:58 -0400

    Implement feature spec user login

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor signs in with account", type: :feature, js: true do
+  before :each do
+    @user = User.create(
+      name: 'Wumbo',
+      email: 'wumbo@gmail.com',
+      password: 'Wumbology',
+      password_confirmation: 'Wumbology'
+    )
+  end
+
+  scenario "They see product details" do
+    # ACT
+    visit root_path
+
+    click_on 'Login'
+    fill_in :email, with: 'wumbo@gmail.com'
+    fill_in :password, with: 'Wumbology'
+    save_screenshot
+    click_on 'Submit'
+
+    expect(page).to have_content('Logout')
+    # DEBUG / VERIFY
+    save_screenshot
+
+  end
+end
